af4e9a649d69a99c2ce7b4012410411284d49b08
Rizqiansyah Ramadhan <thenoblerizz@gmail.com>
2024-01-31 17:11:32 +0700

    build src

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,8 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const helmet_1 = __importDefault(require("helmet"));
-const morgan_1 = __importDefault(require("morgan"));
 require("dotenv/config");
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
-const logger_1 = __importDefault(require("./middleware/logger"));
 const corsConfig_1 = __importDefault(require("./middleware/corsConfig"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 const postRoutes_1 = __importDefault(require("./routes/postRoutes"));
@@ -31,8 +29,8 @@ app.use(credentials_1.default);
 app.use((0, cors_1.default)(corsConfig_1.default));
 app.use((0, helmet_1.default)());
 app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" })); // ! buat image harus begini
-app.use((0, morgan_1.default)("dev"));
-app.use((0, morgan_1.default)("combined", { stream: logger_1.default }));
+// app.use(morgan("dev")); // ! netlify itu readonly jadi gabisa add log otomatis
+// app.use(morgan("combined", { stream: logger }));
 app.use("/assets", express_1.default.static(path_1.default.join(__dirname, "./public/assets")));
 // * Routes
 app.use("/auth", authRoutes_1.default);
