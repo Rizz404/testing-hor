f066b2ecbfb5a8463842f8c19b7b31f7e0809768
Rizqiansyah Ramadhan <thenoblerizz@gmail.com>
2024-02-04 12:20:07 +0700

    fix cors

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -4,7 +4,7 @@ const allowedOrigins: string[] = [
   "http://localhost:5173",
   "http://localhost:8000",
   "http://localhost:8080",
-  "http://127.0.0.1:8000/",
+  "http://127.0.0.1:8000",
 ];
 
 export default allowedOrigins;
--- src/middleware/credentials.ts
+++ src/middleware/credentials.ts
@@ -5,6 +5,7 @@ const credentials: RequestHandler = (req, res, next) => {
   const { origin } = req.headers;
 
   if (allowedOrigins.includes(origin || "")) {
+    res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
